649cd3532154d3bb34056a9a76e6f9f8d4407040
kizitoomoit <kizitoomoit9@gmail.com>
2019-08-11 23:44:44 +0300

    This program reads your name and age on the terminal and it has been written in python

---
+++ read_name_and_age.py
@@ -0,0 +1,11 @@
+# This python programm will read your name and age
+
+print('Hello, wellcome to my program')
+print('What is your name?')
+myName = input()
+print('Nice to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 2) + ' in two years.')
